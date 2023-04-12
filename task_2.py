16a7a3f8a27e59b97b83e324e1bb52a43c7f9cfa
Aashir  Waqar <aashir.waqar@A006-00550.local>
2023-04-12 15:06:15 +0500

    Modified code for pep8

--- task_2.py
+++ task_2.py
@@ -1,10 +1,12 @@
-def task_2(my_dict1,my_dict2):
+def task_2(my_dict1, my_dict2):
     dict3 = my_dict1.copy()
     dict3.update(my_dict2)
     for key in my_dict1:
         if key in my_dict2:
             dict3[key] = my_dict1[key] + my_dict2[key]
     print(dict3)
+
+
 my_dict1 = {1: 100, 2: 200, 3: 300}
 my_dict2 = {1: 100, 5: 200, 9: 300}
-task_2(my_dict1,my_dict2)
+task_2(my_dict1, my_dict2)
