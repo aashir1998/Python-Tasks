f312a03ace2b02c13e0892e3bb5d6788d33bb442
Aashir  Waqar <aashir.waqar@A006-00550.local>
2023-04-17 18:22:42 +0500

    Added line after end of function

--- task_3.py
+++ task_3.py
@@ -1,4 +1,6 @@
 def task_3(arr):
     dict3 = {key: value for key, value in arr.items() if value % 2 == 0}
     print(dict3)
-task_3({'value1': 1, 'value2': 2, 'value3': 3,"value4":8})
+
+
+task_3({"value1": 1, "value2": 2, "value3": 3, "value4": 8})
